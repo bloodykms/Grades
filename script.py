7ea47f782e533caeb8247a29ae98d3645fa911ec
= <=>
2020-06-29 10:38:33 +0100

    Add Calculate_grade() function

--- script.py
+++ script.py
@@ -6,6 +6,35 @@
 student_name = 'Mohamed Sofiane';
 student_id = '20201000071'
 
+def calculate_grade(mark):
+    grade = '' 
+    if mark >= 81:
+        grade = 'Excellent'
+    elif mark >=71:
+        grade = 'Very good'
+    elif mark >=61:
+        grade = 'Good'
+    elif mark >=51:
+        grade = 'Weak'
+    elif mark ==50:
+        grade = 'Pass'
+    else:
+        grade = 'Fail'
+    return grade
+
+###
+
 print ('Name: ',student_name)
 print ('ID: ',student_id)
+
+print ('\nGrades:')
+print ('Python programming 101 :',calculate_grade(87))
+print ('Math :',calculate_grade(81))
+print ('Physics :',calculate_grade(71))
+print ('English :',calculate_grade(50))
+
+
+
+
+###
 
