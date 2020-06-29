a33464b6d46bd8181c8d7f48d7e535b5909616fb
= <=>
2020-06-29 08:57:18 +0100

    Added: Print name and Id

--- script.py
+++ script.py
@@ -2,3 +2,10 @@
     Date            : 29 Juin 2020
     Supervised By   : Kameche Mohamed Sofiane
 '''
+
+student_name = 'Mohamed Sofiane';
+student_id = '20201000071'
+
+print ('Name: ',student_name)
+print ('ID: ',student_id)
+
